5dad3384d8b90666b6121ba08a7ffd3aff78c9af
fengchi <c2h20406@gmail.com>
2016-12-11 23:03:36 +0800

    order by topic.votes.count

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :topics do
   	member do
   		post 'upvote'
+  		post 'downvote'
   	end
   end
   root "topics#index"
+  get 'about' => "topics#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
